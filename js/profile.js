00b37bc12b569bf6220dd6e2e5a400a5a5d1c388
varunvinay4u <varunvinay4u@gmail.com>
2023-02-16 15:15:30 +0530

    additional fields

--- js/profile.js
+++ js/profile.js
@@ -3,3 +3,6 @@ const profileForm = document.querySelector('#profile-form');
 const usernameField = document.querySelector('#username');
 const emailField = document.querySelector('#email');
 const passwordField = document.querySelector('#password');
+const ageField = document.querySelector('#age');
+const editProfileBtn = document.querySelector('#edit-profile-btn');
+const saveProfileBtn = document.querySelector('#save-profile-btn');
