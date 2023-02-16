1a818798fee3ffb0793f9fbd12731cd0a52fbb94
varunvinay4u <varunvinay4u@gmail.com>
2023-02-16 15:14:37 +0530

    profile js file

---
+++ js/profile.js
@@ -0,0 +1,5 @@
+// Get form and fields
+const profileForm = document.querySelector('#profile-form');
+const usernameField = document.querySelector('#username');
+const emailField = document.querySelector('#email');
+const passwordField = document.querySelector('#password');
