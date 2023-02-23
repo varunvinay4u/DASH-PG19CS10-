a6fa89e70592064394fa3f5f9486cedc34fcc4f4
varunvinay4u <varunvinay4u@gmail.com>
2023-02-23 15:58:51 +0530

    edit

--- js/quiz2.js
+++ js/quiz2.js
@@ -11,3 +11,11 @@ answers[4] = "B";
 function SetAnswer(questionNumber, answerSelection) {
     userAnswers[questionNumber] = answerSelection;
 }
+
+function MarkIncorrectQuestions() {
+    for(i = 1; i <= totalQuestions; i++) {
+        if(answers[i] != userAnswers[i]) {
+            document.getElementById(i).className += " wrong";
+        }
+    }
+}
