e9d0b745a2f80c873c6b4e3a90ab3baafc2af072
Swathi K <swathikrmt@gmail.com>
2023-03-06 09:17:21 +0530

    edit

--- js/quiz6.js
+++ js/quiz6.js
@@ -7,3 +7,15 @@ answers[1] = "D";
 answers[2] = "C";
 answers[3] = "B";
 answers[4] = "D";
+
+function SetAnswer(questionNumber, answerSelection) {
+    userAnswers[questionNumber] = answerSelection;
+}
+
+function MarkIncorrectQuestions() {
+    for(i = 1; i <= totalQuestions; i++) {
+        if(answers[i] != userAnswers[i]) {
+            document.getElementById(i).className += " wrong";
+        }
+    }
+}
