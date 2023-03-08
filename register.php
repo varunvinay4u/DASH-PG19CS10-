13e3fbef91e23262eb540ab3dc286a6c49f6a6d9
varunvinay4u <varunvinay4u@gmail.com>
2023-03-08 10:52:34 +0530

    e

--- register.php
+++ register.php
@@ -7,3 +7,10 @@
   <title>Register</title>
   <link rel="stylesheet" href="css/register.css">
 </head>
+
+<body>
+  <div class="container">
+    <h1>Please provide details to complete Registration</h1>
+    <form method="post" name="registration" class="registartion-form" onsubmit="return formValidation()">
+      <table>
+        <tr>
