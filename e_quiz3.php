7461813b5431f8b70151d72b63c5c670bf9cb4c9
varunvinay4u <varunvinay4u@gmail.com>
2023-03-08 12:23:17 +0530

    E

--- e_quiz3.php
+++ e_quiz3.php
@@ -3,3 +3,10 @@
 //  session_start();
 include("connection.php");
 include("functions.php");
+
+$answers = array("A", "D", "C", "B", "B", "A","B","C","D","B"); // correct answers
+$score = 0; // initialize score variable
+
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+$addr=$_POST['addr'];
