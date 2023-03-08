cb4603b42c6abce093982ab998a8af7f483d8daa
SREEHARI-P-S <68457346+SREEHARI-P-S@users.noreply.github.com>
2023-03-09 00:12:55 +0530

    edit

--- js/quiz10.js
+++ js/quiz10.js
@@ -13,3 +13,15 @@ answers[7] = "D";
 answers[8] = "B";
 answers[9] = "A";
 answers[10] = "D";
+
+function SetAnswer(questionNumber, answerSelection) {
+    userAnswers[questionNumber] = answerSelection;
+}
+
+function MarkIncorrectQuestions() {
+    for(i = 1; i <= totalQuestions; i++) {
+        if(answers[i] != userAnswers[i]) {
+            document.getElementById(i).className += " wrong";
+        }
+    }
+}
