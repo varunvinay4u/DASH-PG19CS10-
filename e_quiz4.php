ddae2203090d4230343977ffb217eb84c45b6bda
varunvinay4u <varunvinay4u@gmail.com>
2023-03-08 12:32:01 +0530

    e

--- e_quiz4.php
+++ e_quiz4.php
@@ -10,4 +10,9 @@ $score = 0; // initialize score variable
 if($_SERVER['REQUEST_METHOD'] == "POST")
 {
 $addr=$_POST['addr'];
-// loop through all quest
+// loop through all questions and check user's answers
+for ($i = 1; $i <= 10; $i++) {
+    if (isset($_POST['radiobutton'.$i]) && $_POST['radiobutton'.$i] == $answers[$i-1]) {
+      $score++; // increment score if user's answer is correct
+    }
+  }
