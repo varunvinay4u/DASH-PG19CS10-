ff1e92124c90bb7442bbaa04f304f9c9219adc8b
SREEHARI-P-S <68457346+SREEHARI-P-S@users.noreply.github.com>
2023-03-08 23:48:13 +0530

    edit

---
+++ js/quiz10.js
@@ -0,0 +1,15 @@
+var totalQuestions = 10;
+
+var answers = new Array;
+var userAnswers = new Array;
+
+answers[1] = "B";
+answers[2] = "C";
+answers[3] = "B";
+answers[4] = "B";
+answers[5] = "C";
+answers[6] = "B";
+answers[7] = "C";
+answers[8] = "D";
+answers[9] = "B";
+answers[10] = "C";
