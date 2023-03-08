5d52ff360ccacff9d853e334459ed05a199c579b
SREEHARI-P-S <68457346+SREEHARI-P-S@users.noreply.github.com>
2023-03-08 23:30:02 +0530

    EDIT

---
+++ js/quiz5.js
@@ -0,0 +1,15 @@
+var totalQuestions = 10;
+
+var answers = new Array;
+var userAnswers = new Array;
+
+answers[1] = "C";
+answers[2] = "A";
+answers[3] = "C";
+answers[4] = "D";
+answers[5] = "B";
+answers[6] = "A";
+answers[7] = "D";
+answers[8] = "C";
+answers[9] = "C";
+answers[10] = "B";
