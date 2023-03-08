3588b85f7e8385d5ec7ea30741ddb7874b3c941e
varunvinay4u <varunvinay4u@gmail.com>
2023-03-08 13:40:33 +0530

    EDIT

--- e_quiz7.php
+++ e_quiz7.php
@@ -3,3 +3,7 @@
 //  session_start();
 include("connection.php");
 include("functions.php");
+
+
+$answers = array("A", "B", "C", "A", "B", "C","B","C","B","D"); // correct answers
+$score = 0; // initialize score variable
