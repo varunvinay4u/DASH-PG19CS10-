3d9e094b4596a57029c68c4a41d3cbf7e3bc44fb
varunvinay4u <varunvinay4u@gmail.com>
2023-03-08 11:00:32 +0530

    e

---
+++ js/register.js
@@ -0,0 +1,10 @@
+// Select all input elements for varification
+const userName = document.getElementById("name");
+const age = document.getElementById("age");
+const dob = document.getElementById("dob");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmpassword = document.getElementById("confirmpassword");
+const phoneNumber = document.getElementById("phoneNumber");
+const gender = document.registration;
+const zipcode = document.getElementById("zipcode");
