5407bc9cee826c2b27e6c1a0e39c6c96caad220b
varunvinay4u <varunvinay4u@gmail.com>
2023-03-08 11:33:56 +0530

    e

--- e_quiz2.php
+++ e_quiz2.php
@@ -6,3 +6,13 @@ include("functions.php");
 
 $answers = array("A", "C", "B", "B", "C", "B","B","A","C","A"); // correct answers
 $score = 0; // initialize score variable
+
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+$addr=$_POST['addr'];
+// loop through all questions and check user's answers
+for ($i = 1; $i <= 10; $i++) {
+  if (isset($_POST['radiobutton'.$i]) && $_POST['radiobutton'.$i] == $answers[$i-1]) {
+    $score++; // increment score if user's answer is correct
+  }
+}
