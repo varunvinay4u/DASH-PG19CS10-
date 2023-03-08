5a401b67e9b111ada3824f0dc6dfaa3c0582d798
varunvinay4u <varunvinay4u@gmail.com>
2023-03-08 11:05:26 +0530

    ans

--- e_quiz1.php
+++ e_quiz1.php
@@ -3,3 +3,7 @@
 //  session_start();
 include("connection.php");
 include("functions.php");
+
+
+$answers = array("A", "C", "B", "B", "C", "B","C","D","B","C"); // correct answers
+$score = 0; // initialize score variable
