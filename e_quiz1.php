a3f8d22d68235af5435332e56ed5a45f48bdf581
varunvinay4u <varunvinay4u@gmail.com>
2023-03-08 11:05:38 +0530

    e

--- e_quiz1.php
+++ e_quiz1.php
@@ -7,3 +7,7 @@ include("functions.php");
 
 $answers = array("A", "C", "B", "B", "C", "B","C","D","B","C"); // correct answers
 $score = 0; // initialize score variable
+
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+$addr=$_POST['addr'];
