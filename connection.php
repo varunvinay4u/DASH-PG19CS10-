915e1bb5cda524b0b661e5b3af0eeada6b8fdf18
varunvinay4u <varunvinay4u@gmail.com>
2023-03-08 10:50:27 +0530

    e

--- connection.php
+++ connection.php
@@ -4,3 +4,14 @@ $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
 $dbname = "dash";
+
+
+$con = new mysqli("$dbhost","$dbuser","$dbpass","$dbname");
+
+// Check connection
+if ($con -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $con -> connect_error;
+  exit();
+}
+
+?>
