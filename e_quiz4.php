be92e0686fdd61a6a20592c08c63d276a1784e54
varunvinay4u <varunvinay4u@gmail.com>
2023-03-08 12:33:14 +0530

    e

--- e_quiz4.php
+++ e_quiz4.php
@@ -19,7 +19,7 @@ for ($i = 1; $i <= 10; $i++) {
 
   
 
-$sql = "update data set l1=$score where id=6";
+$sql = "update data set alphabet=$score where id=6";
 if ($con->query($sql) === TRUE) {
   // echo "New record created successfully";
 }
