1c6680d576318517d0227d526347c24dd1aad4e5
Swathi K <swathikrmt@gmail.com>
2023-06-21 18:42:32 +0530

    edits

--- lesson1.php
+++ lesson1.php
@@ -17,6 +17,7 @@
 	<script src="js/lesson.js"></script>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+	<link rel="icon" href="image/logo1.png" type="image/png">
 	<style>
 		body {
 			background-image: url(image/bg.jpg);
