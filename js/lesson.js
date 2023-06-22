868c4add1873155aa8488d8d3970fedc7b3c7d03
Swathi K <91047224+swathi-athi@users.noreply.github.com>
2023-06-23 00:02:58 +0530

    Update lesson.js

--- js/lesson.js
+++ js/lesson.js
@@ -12,27 +12,3 @@ function showContent(sectionId) {
         }
     }
 }
-
-// fetch data from txt
-document.addEventListener("DOMContentLoaded", function() {
-    fetchContent();
-});
-
-function fetchContent() {
-    fetch("data/data.txt")
-        .then(response => response.text())
-        .then(data => {
-            const sections = data.split("---");
-
-            const section1Content = sections[0].trim();
-            const section2Content = sections[1].trim();
-            const section3Content = sections[2].trim();
-
-            document.getElementById("section1-content").textContent = section1Content;
-            document.getElementById("section2-content").textContent = section2Content;
-            document.getElementById("section3-content").textContent = section3Content;
-        })
-        .catch(error => {
-            console.error("Error fetching content:", error);
-        });
-}
